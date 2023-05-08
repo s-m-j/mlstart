751e2d3bfa466bf363ab5b41a24a44eb5b8a3d7b
Sergey Maslov <sergey.s.maslov@gmail.com>
2023-05-08 23:25:38 +0300

    bug fixed

--- web/js/display.js
+++ web/js/display.js
@@ -1,70 +1,72 @@
-function createRow(container, studentName, samples){
-    const row=document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
+function createRow(container, studentName, samples) {
+  const row = document.createElement("div");
+  row.classList.add("row");
+  container.appendChild(row);
 
-    const rowLabel=document.createElement("div");
-    rowLabel.innerHTML=studentName;
-    rowLabel.classList.add("rowLabel");
-    row.appendChild(rowLabel);
+  const rowLabel = document.createElement("div");
+  rowLabel.innerHTML = studentName;
+  rowLabel.classList.add("rowLabel");
+  row.appendChild(rowLabel);
 
-    for(let sample of samples){
-        const {id, label,student_id}=sample;
+  for (let sample of samples) {
+    const { id, label, student_id } = sample;
 
-        const sampleContainer=document.createElement("div");
-        sampleContainer.id="sample_"+id;
-        sampleContainer.onclick=()=>{
-            handleClick(sample, false);
-        }
-        sampleContainer.classList.add("sampleContainer");
+    const sampleContainer = document.createElement("div");
+    sampleContainer.id = "sample_" + id;
+    sampleContainer.onclick = () => {
+      handleClick(sample, false);
+    };
+    sampleContainer.classList.add("sampleContainer");
 
-        const sampleLabel=document.createElement("div");
-        sampleLabel.innerHTML=label;
-        sampleContainer.appendChild(sampleLabel);
+    const sampleLabel = document.createElement("div");
+    sampleLabel.innerHTML = label;
+    sampleContainer.appendChild(sampleLabel);
 
-        const img=document.createElement("img");
-        img.src=constants.IMG_DIR+'/'+id+".png";
-        img.classList.add("thumb");
-        if(utils.flaggedUsers.includes(student_id)){
-            img.classList.add("blur");
-        }
-        sampleContainer.appendChild(img);
-
-        row.appendChild(sampleContainer);
+    const img = document.createElement("img");
+    img.src = constants.IMG_DIR + "/" + id + ".png";
+    img.classList.add("thumb");
+    if (utils.flaggedUsers.includes(student_id)) {
+      img.classList.add("blur");
     }
+    sampleContainer.appendChild(img);
+
+    row.appendChild(sampleContainer);
+  }
 }
 
-function handleClick(sample,doScroll=TransformStreamDefaultController){
-    if(sample==null){
-        [...document.querySelectorAll('.emphasize')].
-            forEach((e)=>e.classList.remove('emphasize'));
-    }
-    
-    const el=document.getElementById("sample_"+sample.id);
+function handleClick(sample, doScroll = TransformStreamDefaultController) {
+  if (sample == null) {
+    [...document.querySelectorAll(".emphasize")].forEach((e) =>
+      e.classList.remove("emphasize")
+    );
+  }
 
-    if(el.classList.contains("emphasize")){
-        el.classList.remove("emphasize");
-        Chart.selectSample(null);
-    }else{
-        [...document.querySelectorAll('.emphasize')].
-            forEach((e)=>e.classList.remove('emphasize'));
-        el.classList.add("emphasize");
-        if(doScroll){
-            el.scrollIntoView({
-                behavoir:'auto',
-                block:'center'
-            })
-        }
-        Chart.selectSample(sample);
+  const el = document.getElementById("sample_" + sample.id);
+
+  if (el.classList.contains("emphasize")) {
+    el.classList.remove("emphasize");
+    Chart.selectSample(null);
+  } else {
+    [...document.querySelectorAll(".emphasize")].forEach((e) =>
+      e.classList.remove("emphasize")
+    );
+    el.classList.add("emphasize");
+    if (doScroll) {
+      el.scrollIntoView({
+        behavoir: "auto",
+        block: "center",
+      });
     }
+    chart.selectSample(sample);
+  }
 }
 
-function toggleInput(){
-    if(inputContainer.style.display=="none"){
-        inputContainer.style.display="block";
-        SketchPad.triggerUpdates();
-    }else{
-        inputContainer.style.display="none";
-        chart.hideDynamicPoint();
-    }
+function toggleInput() {
+  if (inputContainer.style.display == "none") {
+    inputContainer.style.display = "block";
+    SketchPad.triggerUpdates();
+  } else {
+    inputContainer.style.display = "none";
+    chart.hideDynamicPoint();
+  }
 }
